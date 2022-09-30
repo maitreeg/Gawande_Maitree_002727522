8d0b72abff8aa22b73b5fa27f3e8fab3acfa2fb5
maitreeg <114595017+maitreeg@users.noreply.github.com>
2022-09-29 23:51:06 -0400

    Add files via upload
    CRUD operations performed.

---
+++ AedAssignment1/src/main/java/com/mycompany/aedass2/AedAss2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aedass2;
+
+import com.aed.view.MainFrame2;
+
+/**
+ *
+ * @author mg
+ */
+public class AedAss2 {
+    
+    public static void main(String[] args) {
+        new MainFrame2().setVisible(true);
+    }
+}
